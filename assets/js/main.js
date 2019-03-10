340d2e04fef9192c0f833da05775e94ef6cbe778
tayyabroy786 <tayyabroy786@gmail.com>
2019-03-10 21:49:05 +0500

    final code with new slider layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,11 +33,37 @@ $('.ex-category-slider').slick({
 $('.ms-slider').slick({
   dots: false,
   infinite: true,
-  speed: 1300,
+  centerMode: true,
+  centerPadding: "0px",
+  speed: 1100,
   draggable: false,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
+  variableWidth: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 667,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
+
+
 /*===========================
     Theme Functions
 =============================*/
